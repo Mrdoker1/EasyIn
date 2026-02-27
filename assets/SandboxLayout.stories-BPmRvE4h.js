e91db089d57dbdfdd799aa38d4dac8073af3cd89
Viachas Kul <fixrapdoK@gmail.com>
2026-02-27 17:33:31 +0300

    Updates

--- assets/SandboxLayout.stories-BPmRvE4h.js
+++
@@ -1,3 +0,0 @@
-import{S as a}from"./SandboxLayout-BuuzBE-n.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-Bk1pUD54.js";import"./preload-helper-PPVm8Dsz.js";import"./LinkedInProfile-CsUCt5Fa.js";import"./ExtensionSidebar-0IshSztz.js";const p={title:"LinkedInSandbox/SandboxLayout",component:a,parameters:{layout:"fullscreen"},tags:["autodocs"]},o={args:{}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
-  args: {}
-}`,...o.parameters?.docs?.source}}};const c=["Default"];export{o as Default,c as __namedExportsOrder,p as default};
---
+++ assets/SandboxLayout.stories-Dp2qWeHx.js
@@ -0,0 +1,3 @@
+import{S as a}from"./SandboxLayout-CH9D6O0Y.js";import"./jsx-runtime-u17CrQMm.js";import"./iframe-Ib95v5Kk.js";import"./preload-helper-CPNoDHzN.js";import"./LinkedInProfile-BqJ6t406.js";import"./ExtensionSidebar-CMnNQjsq.js";const p={title:"LinkedInSandbox/SandboxLayout",component:a,parameters:{layout:"fullscreen"},tags:["autodocs"]},o={args:{}};o.parameters={...o.parameters,docs:{...o.parameters?.docs,source:{originalSource:`{
+  args: {}
+}`,...o.parameters?.docs?.source}}};const c=["Default"];export{o as Default,c as __namedExportsOrder,p as default};
